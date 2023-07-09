bec06c905adac8a7fb96ff48cf2b1568cb10284f
serkanguentin <guntinfurkan@gmail.com>
2023-07-09 16:04:56 +0200

    sekond  C commit

---
+++ C_Programmirung_Aufgabe4_Float.c
@@ -0,0 +1,13 @@
+// ML_01_02_02
+
+#include <stdio.h>
+
+main()
+{
+	float a=1.2345;
+	float b=2.3456;
+	float c=3.4567;
+	float summe=a+b+c;
+
+	printf("%.4f + %.4f + %.4f = %.4f",a,b,c,summe);
+}
---
+++ C_Programmirung_Aufgabe5_IntegerUndFloat.c
@@ -0,0 +1,15 @@
+// ML_01_02_03
+
+#include <stdio.h>
+
+main()
+{
+	int alter=45;
+	float groesse=1.82;
+
+	printf("\n\n\tName: Hans Mueller\n"); // Die Ausgabe von Umlauten, ß und Eurozeichen lernen wir erst etwas später
+	printf("\tAlter: %d\n",alter);
+	printf("\tGroesse: %.2f m",groesse); // Die Ausgabe von Umlauten, ß und Eurozeichen lernen wir erst etwas später
+
+	printf("\n\n\n"); // sorgt für Abstand zu der stets erscheinenden "automatierten IDE-Ausgabe" ... auch dazu demnächst mehr ...
+}
